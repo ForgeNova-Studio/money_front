a48bac5ea42dd65192e53571ac353da4c10cbefc
Chohanwool <hanwoolc95@gmail.com>
2025-12-19 17:57:46 +0900

    feat-income: (Domain) income_repository 추상 클래스 작성

---
+++ lib/features/income/domain/repositories/income_repository.dart
@@ -0,0 +1,96 @@
+// entities
+import 'package:moneyflow/features/income/domain/entities/income_model.dart';
+
+/// Income Repository 인터페이스
+///
+/// Income 관련 비즈니스 로직의 추상화 계층
+/// Data Layer에서 이 인터페이스를 구현
+///
+/// Exception 처리:
+/// - NetworkException: 네트워크 연결 오류
+/// - UnauthorizedException: 인증 실패 (잘못된 이메일/비밀번호)
+/// - ValidationException: 입력값 검증 오류
+/// - ServerException: 서버 오류 (500번대)
+/// - TokenExpiredException: 토큰 만료
+abstract class IncomeRepository {
+  /// 수입 목록 조회
+  /// [startDate] 조회 시작일*
+  /// [endDate] 조회 종료일*
+  /// [source] 수입 출처
+  ///
+  /// Returns: [List<IncomeModel>] 수입 목록
+  ///
+  /// Throws:
+  /// - [NetworkException] 네트워크 오류
+  /// - [TokenExpiredException] 토큰 만료
+  /// - [ServerException] 서버 오류
+  Future<List<IncomeModel>> getIncomeList({
+    required DateTime startDate,
+    required DateTime endDate,
+    String? source,
+  });
+
+  /// 수입 생성
+  /// [income] 수입 정보
+  ///
+  /// Returns: [IncomeModel] 생성된 수입
+  ///
+  /// Throws:
+  /// - [NetworkException] 네트워크 오류
+  /// - [TokenExpiredException] 토큰 만료
+  /// - [ServerException] 서버 오류
+  Future<IncomeModel> createIncome({
+    required IncomeModel income,
+  });
+
+  /// 수입 상세 조회
+  /// [incomeId] 수입 ID*
+  ///
+  /// Returns: [IncomeModel]
+  ///
+  /// Throws:
+  /// - [NetworkException] 네트워크 오류
+  /// - [TokenExpiredException] 토큰 만료
+  /// - [ServerException] 서버 오류
+  Future<IncomeModel> getIncomeDetail({
+    required String incomeId,
+  });
+
+  /// 수입 수정
+  /// [incomeId] 수입 ID*
+  /// [income] 수입 정보*
+  ///
+  /// Returns: [IncomeModel] 수정된 수입
+  ///
+  /// Throws:
+  /// - [NetworkException] 네트워크 오류
+  /// - [TokenExpiredException] 토큰 만료
+  /// - [ServerException] 서버 오류
+  Future<IncomeModel> updateIncome({
+    required String incomeId,
+    required IncomeModel income,
+  });
+
+  /// 수입 삭제
+  /// [incomeId] 수입 ID*
+  ///
+  /// Returns: void
+  ///
+  /// Throws:
+  /// - [NetworkException] 네트워크 오류
+  /// - [TokenExpiredException] 토큰 만료
+  /// - [ServerException] 서버 오류
+  Future<void> deleteIncome({
+    required String incomeId,
+  });
+
+  /// 최근 수입 내역 조회 (최근 수입 5개 조회 - 홈 화면 전용)
+  ///
+  /// Returns: [List<IncomeModel>] 최근 수입 목록
+  ///
+  /// Throws:
+  /// - [NetworkException] 네트워크 오류
+  /// - [TokenExpiredException] 토큰 만료
+  /// - [ServerException] 서버 오류
+  Future<List<IncomeModel>> getRecentIncomeList();
+}
