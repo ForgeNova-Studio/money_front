fc73e9cd2b8c130710de732dfef905e7e0b5a48b
Chohanwool <hanwoolc95@gmail.com>
2025-12-22 13:23:36 +0900

    feat-income: (Data) remote_datasource 추상 클래스 작성

---
+++ lib/features/income/data/datasources/income_remote_datasource.dart
@@ -0,0 +1,31 @@
+import 'package:moneyflow/features/income/data/models/income_model.dart';
+
+/// Income Remote Data Source
+///
+/// Dio를 사용한 API 통신 구현
+/// - 순수 Dart 인터페이스
+/// - Data Model 반환
+/// - 구현체에서 예외 처리
+abstract class IncomeRemoteDataSource {
+  /// 수입 목록 조회
+  Future<List<IncomeModel>> getIncomeList({
+    required DateTime startDate,
+    required DateTime endDate,
+    String? source,
+  });
+
+  /// 수입 생성
+  Future<IncomeModel> createIncome({required IncomeModel income});
+
+  /// 수입 상세 조회
+  Future<IncomeModel> getIncome({required String incomeId});
+
+  /// 수입 수정
+  Future<IncomeModel> updateIncome({
+    required String incomeId,
+    required IncomeModel income,
+  });
+
+  /// 수입 삭제
+  Future<void> deleteIncome({required String incomeId});
+}
