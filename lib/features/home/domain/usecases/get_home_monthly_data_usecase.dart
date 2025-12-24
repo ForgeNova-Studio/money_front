297178eaf458eee2de890410d099319a32831354
Chohanwool <hanwoolc95@gmail.com>
2025-12-24 16:03:45 +0900

    feat-home: (Domain) usecase 작성

---
+++ lib/features/home/domain/usecases/get_home_monthly_data_usecase.dart
@@ -0,0 +1,14 @@
+import 'package:moneyflow/features/home/domain/entities/daily_transaction_summary.dart';
+import 'package:moneyflow/features/home/domain/repositories/home_repository.dart';
+
+class GetHomeMonthlyDataUseCase {
+  final HomeRepository _repository;
+
+  GetHomeMonthlyDataUseCase(this._repository);
+
+  Future<Map<String, DailyTransactionSummary>> call({
+    required DateTime yearMonth,
+  }) async {
+    return await _repository.getMonthlyHomeData(yearMonth: yearMonth);
+  }
+}
