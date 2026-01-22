5179bc1a43bd00496996a49c6eb0fb55147e5a22
stringT <th8260@naver.com>
2026-01-22 15:39:24 +0900

    feat: 오라클 서버 셋팅

--- lib/core/constants/api_constants.dart
+++ lib/core/constants/api_constants.dart
@@ -3,7 +3,7 @@ class ApiConstants {
   // 빌드 시 --dart-define=API_BASE_URL=... 로 주입
   static const String baseUrl = String.fromEnvironment(
     'API_BASE_URL',
-    defaultValue: 'http://192.168.123.104:8080',
+    defaultValue: 'http://158.179.166.233:80',
   );
 
   // ========== Auth endpoints ==========
